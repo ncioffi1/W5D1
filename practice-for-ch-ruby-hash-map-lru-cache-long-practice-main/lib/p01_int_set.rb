8c0153186f134f1829ab6477f9b203058bb9c5af
Mujahed Ali <mujahed97@gmail.com>
2023-11-13 16:52:32 -0500

    finished int set

--- practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p01_int_set.rb
+++ practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p01_int_set.rb
@@ -1,14 +1,25 @@
-class MaxIntSet
+class MaxIntSet # [0, 1, 2, 3]
+  attr_reader :store
+
   def initialize(max)
+    @max = max
+    @store = Array.new(max, false)
   end
 
   def insert(num)
+    if num > @max || num < 0
+      raise 'Out of bounds'
+    else 
+      @store[num] = true
+    end
   end
 
   def remove(num)
+    @store[num] = false
   end
 
   def include?(num)
+    return @store[num] == true
   end
 
   private
@@ -21,17 +32,21 @@ class MaxIntSet
 end
 
 class IntSet
-  def initialize(num_buckets = 20)
-    @store = Array.new(num_buckets) { Array.new }
+  def initialize(num_buckets = 20) [0, 1 , 2, 3]
+    @store = Array.new(num_buckets) {Array.new}
   end
 
   def insert(num)
+    @store[num % @store.length] << num
   end
 
   def remove(num)
+    index = @store[num % @store.length].index(num)
+    @store[num % @store.length].delete_at(index)
   end
 
   def include?(num)
+    @store[num % @store.length].include?(num)
   end
 
   private
@@ -54,12 +69,23 @@ class ResizingIntSet
   end
 
   def insert(num)
+    if !self.include?(num)
+     self[num] << num
+     @count += 1
+    end
+    resize! if @store.length < @count
   end
 
   def remove(num)
+    if self.include?(num)
+      index = self[num].index(num)
+      self[num].delete_at(index)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
@@ -69,9 +95,20 @@ class ResizingIntSet
   end
 
   def resize!
+    previous = @store.dup
+    new_size = @store.length * 2
+    @store = Array.new(new_size) { Array.new }
+
+    previous.each do |bucket|
+      bucket.each do |ele|
+        @store[ele % @store.length] << ele 
+      end
+    end
+
   end
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % @store.length]
   end
 end
